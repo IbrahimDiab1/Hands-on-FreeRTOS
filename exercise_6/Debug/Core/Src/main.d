62c558962d265880a6676d63da4e59c01fd4e30a
Ibrahim_Diab <ibrahim0diab@gmail.com>
2024-06-12 07:47:31 +0300

    Switch tasks priorities through external interrupt

---
+++ exercise_6/Debug/Core/Src/main.d
@@ -0,0 +1,68 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ ../Core/Inc/stm32f1xx_hal_conf.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ ../Drivers/CMSIS/Include/core_cm3.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h
+../Core/Inc/main.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+../Core/Inc/stm32f1xx_hal_conf.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+../Drivers/CMSIS/Include/core_cm3.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h:
---
+++ exercise_6/Debug/Third_Party/FreeRTOS/event_groups.d
@@ -0,0 +1,28 @@
+Third_Party/FreeRTOS/event_groups.o: \
+ ../Third_Party/FreeRTOS/event_groups.c \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/timers.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/event_groups.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/timers.h
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/timers.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/event_groups.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/timers.h:
