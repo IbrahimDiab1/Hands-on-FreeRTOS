3c05cc2f15ba260975a7d4dae33860521cf7471a
Ibrahim_Diab <ibrahim0diab@gmail.com>
2024-06-29 07:37:37 +0300

    Mutual exclusion using a mutex for UART communication between tasks

---
+++ exercise_9/Debug/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/port.d
@@ -0,0 +1,20 @@
+Third_Party/FreeRTOS/portable/GCC/ARM_CM3/port.o: \
+ ../Third_Party/FreeRTOS/portable/GCC/ARM_CM3/port.c \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h \
+ ../Core/Inc/FreeRTOSConfig.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h \
+ D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/FreeRTOS.h:
+../Core/Inc/FreeRTOSConfig.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/projdefs.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/portable.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/deprecated_definitions.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/portable/GCC/ARM_CM3/portmacro.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/mpu_wrappers.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/task.h:
+D:/My\ Temp\ Files/Temp_coding_Simulating/FREE_RTOS/exercise_3/Third_Party/FreeRTOS/include/list.h:
